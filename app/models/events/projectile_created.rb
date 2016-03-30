d3c35acd4a8a9ca4ca5fc84d9dff96a021e67f37
Björn Dahlgren <bjorn@dahlgren.at>
2016-03-31 01:34:25 +0200

    Vehicles can also create projectiles

--- app/models/events/projectile_created.rb
+++ app/models/events/projectile_created.rb
@@ -1,6 +1,15 @@
 class Events::ProjectileCreated < Event
   embeds_one :projectile, class_name: 'Schemas::Projectile'
   embeds_one :unit, class_name: 'Schemas::Unit'
+  embeds_one :vehicle, class_name: 'Schemas::Vehicle'
 
-  validates_presence_of :projectile, :unit
+  validates_presence_of :projectile
+
+  def validate_embedded_record_for_unit
+    unit = nil unless unit && unit.valid?
+  end
+
+  def validate_embedded_record_for_vehicle
+    vehicle = nil unless vehicle && vehicle.valid?
+  end
 end
